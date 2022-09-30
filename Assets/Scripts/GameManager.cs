223cdc1d9a43c91b6eb99618dea14eda2ad28271
Dragonfactor <dorian.scrimshaw@student.sl.on.ca>
2022-09-30 17:14:36 -0400

    End of chapter 5

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -34,34 +33,40 @@ public class GameManager : MonoBehaviour
         if (aliensPerSpawn > 0 && aliensOnScreen < totalAliens)
         {
             List<int> previousSpawnLocations = new List<int>();
-        }
-        if (aliensPerSpawn > spawnPoints.Length)
-        {
-            aliensPerSpawn = spawnPoints.Length - 1;
-        }
-        aliensPerSpawn = (aliensPerSpawn > totalAliens) ?
-        aliensPerSpawn - totalAliens : aliensPerSpawn;
-        for (int i = 0; i < aliensPerSpawn; i++)
-        {
-            if (aliensOnScreen < maxAliensOnScreen)
+
+            if (aliensPerSpawn > spawnPoints.Length)
             {
-                aliensOnScreen += 1;
-                int spawnPoint = -1;
-                
-                while (spawnPoint == -1)
+                aliensPerSpawn = spawnPoints.Length - 1;
+            }
+            aliensPerSpawn = (aliensPerSpawn > totalAliens) ?
+            aliensPerSpawn - totalAliens : aliensPerSpawn;
+            for (int i = 0; i < aliensPerSpawn; i++)
+            {
+                if (aliensOnScreen < maxAliensOnScreen)
                 {
-                    
-                    int randomNumber = Random.Range(0, spawnPoints.Length - 1);
-                    
-                    if (!previousSpawnLocations.Contains(randomNumber))
+                    aliensOnScreen += 1;
+                    int spawnPoint = -1;
+
+                    while (spawnPoint == -1)
                     {
-                        previousSpawnLocations.Add(randomNumber);
-                        spawnPoint = randomNumber;
+
+                        int randomNumber = Random.Range(0, spawnPoints.Length - 1);
+
+                        if (!previousSpawnLocations.Contains(randomNumber))
+                        {
+                            previousSpawnLocations.Add(randomNumber);
+                            spawnPoint = randomNumber;
+                        }
                     }
+                    GameObject spawnLocation = spawnPoints[spawnPoint];
+                    GameObject newAlien = Instantiate(alien) as GameObject;
+                    newAlien.transform.position = spawnLocation.transform.position;
+                    Alien alienScript = newAlien.GetComponent<Alien>();
+                    alienScript.target = player.transform;
+                    Vector3 targetRotation = new Vector3(player.transform.position.x,newAlien.transform.position.y, player.transform.position.z);
+                    newAlien.transform.LookAt(targetRotation);
+
                 }
-                GameObject spawnLocation = spawnPoints[spawnPoint];
-                GameObject newAlien = Instantiate(alien) as GameObject;
-                newAlien.transform.position = spawnLocation.transform.position;
             }
         }
     }
